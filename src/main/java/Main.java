bbf6b7dc47d94fc3ba72afd4320f7815d3fcfa8f
Andrey Kirienko <kirienko.av@outlook.com>
2019-06-27 16:42:42 +1000

    add options support

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,33 @@
+import org.apache.commons.cli.*;
 import service.AvailabilityIntervalService;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(
-                ("192.168.32.181 - - [14/06/2017:16:46:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=6076537c HTTP/1.1\" 200 2 44.510983 \"-\" \"@list-item-updater\" prio:0\n" +
-                        "192.168.32.181 - - [14/06/2017:16:47:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=6076537c HTTP/1.1\" 200 2 44.510983 \"-\" \"@list-item-updater\" prio:0\n" +
-                        "192.168.32.181 - - [14/06/2017:16:47:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=7ae28555 HTTP/1.1\" 200 2 23.251219 \"-\" \"@list-item-updater\" prio:0\n" +
-                        "192.168.32.181 - - [14/06/2017:16:47:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=7ae28555 HTTP/1.1\" 200 2 23.251219 \"-\" \"@list-item-updater\" prio:0\n" +
-                        "192.168.32.181 - - [14/06/2017:16:47:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=7ae28555 HTTP/1.1\" 200 2 33.251219 \"-\" \"@list-item-updater\" prio:0\n" +
-                        "192.168.32.181 - - [14/06/2017:16:47:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=e356713 HTTP/1.1\" 200 2 21.164372 \"-\" \"@list-item-updater\" prio:0").getBytes())));
+        Double maxRuntime = 45D;
+        Double minAccessLevel = 99.9;
+        Options options = new Options();
+        options.addOption("u", true, "Minimum acceptable level of availability.");
+        options.addOption("t", true, "Acceptable response time (milliseconds).");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        AvailabilityIntervalService analyzer = new AvailabilityIntervalService(25D, 99D);
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine cmd = parser.parse(options, args);
+            maxRuntime = Optional.ofNullable(cmd.getOptionValue("t"))
+                    .map(Double::parseDouble)
+                    .orElse(maxRuntime);
+            minAccessLevel = Optional.ofNullable(cmd.getOptionValue("u"))
+                    .map(Double::parseDouble)
+                    .orElse(minAccessLevel);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        AvailabilityIntervalService analyzer = new AvailabilityIntervalService(maxRuntime, minAccessLevel);
         analyzer.process(bufferedReader.lines())
                 .forEach(System.out::println);
     }
