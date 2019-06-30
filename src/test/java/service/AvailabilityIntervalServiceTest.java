dd52d11d995c70d5e719a752a1aaa7e82584fd00
Andrey Kirienko <kirienko.av@outlook.com>
2019-06-30 23:55:52 +1000

    addition tests

--- src/test/java/service/AvailabilityIntervalServiceTest.java
+++ src/test/java/service/AvailabilityIntervalServiceTest.java
@@ -4,6 +4,8 @@ import model.AvailabilityInterval;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
@@ -21,35 +23,7 @@ class AvailabilityIntervalServiceTest {
     @Test
     @DisplayName("process test")
     void processTest() {
-        Stream<String> lines = Stream.of("192.168.32.181 - - [14/06/2017:16:47:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=6076537c HTTP/1.1\" 200 2 44.510983 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:47:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=7ae28555 HTTP/1.1\" 500 2 23.251219 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:47:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=e356713 HTTP/1.1\" 200 2 56.164372 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:47:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=6c21c8f6 HTTP/1.1\" 200 2 56.02583 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:47:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=cceed874 HTTP/1.1\" 200 2 35.249855 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:47:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=4b84a53c HTTP/1.1\" 200 2 56.783072 \"-\" \"@list-item-updater\" prio:0",
-
-                "192.168.32.181 - - [14/06/2017:16:48:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=6076537c HTTP/1.1\" 200 2 44.510983 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:48:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=7ae28555 HTTP/1.1\" 200 2 23.251219 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:48:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=e356713 HTTP/1.1\" 200 2 23.164372 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:48:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=6c21c8f6 HTTP/1.1\" 200 2 23.02583 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:48:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=cceed874 HTTP/1.1\" 200 2 35.249855 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:48:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=4b84a53c HTTP/1.1\" 200 2 23.783072 \"-\" \"@list-item-updater\" prio:0",
-
-                "192.168.32.181 - - [14/06/2017:16:49:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=6076537c HTTP/1.1\" 200 2 44.510983 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:49:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=6c21c8f6 HTTP/1.1\" 500 2 23.02583 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:49:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=cceed874 HTTP/1.1\" 200 2 35.249855 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:49:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=4b84a53c HTTP/1.1\" 200 2 23.783072 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:49:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=6076537c HTTP/1.1\" 200 2 44.510983 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:50:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=7ae28555 HTTP/1.1\" 200 2 23.251219 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:50:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=e356713 HTTP/1.1\" 200 2 23.164372 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:50:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=6c21c8f6 HTTP/1.1\" 500 2 23.02583 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:50:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=cceed874 HTTP/1.1\" 200 2 35.249855 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:50:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=4b84a53c HTTP/1.1\" 200 2 23.783072 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:51:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=7ae28555 HTTP/1.1\" 200 2 23.251219 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:51:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=e356713 HTTP/1.1\" 200 2 23.164372 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:51:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=6c21c8f6 HTTP/1.1\" 500 2 23.02583 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:51:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=cceed874 HTTP/1.1\" 200 2 35.249855 \"-\" \"@list-item-updater\" prio:0",
-                "192.168.32.181 - - [14/06/2017:16:51:02 +1000] \"PUT /rest/v1.4/documents?zone=default&_rid=4b84a53c HTTP/1.1\" 200 2 23.783072 \"-\" \"@list-item-updater\" prio:0");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/test1.log")));
 
         AvailabilityInterval availabilityInterval1 = new AvailabilityInterval();
         availabilityInterval1.setStartDate(OffsetDateTime.of(LocalDateTime.of(2017, 06, 14, 16, 47, 02),
@@ -68,7 +42,15 @@ class AvailabilityIntervalServiceTest {
         availabilityInterval2.setFailureCount(3L);
 
         List<AvailabilityInterval> expected = Arrays.asList(availabilityInterval1, availabilityInterval2);
-        List<AvailabilityInterval> actual = availabilityIntervalService.process(lines).collect(Collectors.toList());
+        List<AvailabilityInterval> actual = availabilityIntervalService.process(bufferedReader.lines()).collect(Collectors.toList());
+        assertIterableEquals(expected, actual);
+
+        bufferedReader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/test2.log")));
+        actual = availabilityIntervalService.process(bufferedReader.lines()).collect(Collectors.toList());
+        assertIterableEquals(expected, actual);
+
+        bufferedReader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/test3.log")));
+        actual = availabilityIntervalService.process(bufferedReader.lines()).collect(Collectors.toList());
         assertIterableEquals(expected, actual);
     }
 
